8f0726fda6093463a78fb1923e7b6f903601601d
Rodrigo Fagundes Coruja <rodrigofagundescoruja@gmail.com>
2023-10-01 17:42:00 -0300

    feat: add ubuntu and rancho font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { ubuntu } from '@/app/fonts'
 
 export const metadata: Metadata = {
   title: 'Contatinhos',
@@ -16,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={ `${ubuntu.className}` }>{children}</body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import { rancho } from '@/app/fonts'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-rancho">
+      <div className={ `${rancho.className}` }>
         opa
       </div>
     </main>
