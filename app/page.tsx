488bd04227198db0f4e7635fd1a7f284e5af5623
Rodrigo Fagundes Coruja <rodrigofagundescoruja@gmail.com>
2023-10-22 18:22:55 -0300

    add colors to dark mode on input text

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 'use client'
 
+import React, { useState } from 'react'
 import { rancho } from '@/app/fonts'
 import TextInput from '@/app/components/TextInput'
 
 export default function Home() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
   return (
     <main className="h-screen flex justify-center items-center">
-      <div className='bg-white dark:bg-brown dark:text-white px-16 py-8 rounded-lg'>
+      <div className='bg-white dark:bg-brown-3 dark:text-white px-16 py-8 rounded-lg flex flex-col gap-8'>
         <h1 className={ `${rancho.className} text-4xl`}>
           Novos contatinhos estão por vir!
         </h1>
 
-        <TextInput placeholder='aaaa' value='' onChange={() => console.log('opa')} />
+        <TextInput placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
+
+        <TextInput type='password' placeholder='Senha' value={password} onChange={(e) => setPassword(e.target.value)} />
       </div>
     </main>
   )
